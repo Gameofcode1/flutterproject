2122565c20223b8d6a7a5b83c83970b9b922bde4
saugat pudasaini <pudasaini.saugatt@gmail.com>
2021-03-23 13:43:54 +0545

    new changes

--- lib/profile/widgets/newcard.dart
+++ lib/profile/widgets/newcard.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../constants.dart';
 
-
 class NewCard extends StatelessWidget {
   final IconData icons;
+
   final String text;
   final IconData nexticons;
 
@@ -15,45 +15,55 @@ class NewCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final data = MediaQuery.of(context);
     return Padding(
-      padding:  EdgeInsets.all(data.size.height/455,),
-      child: Card(
-          shape: kCardShape,
-          elevation: 1.0,
-          child: Padding(
-            padding: EdgeInsets.only(top:data.size.height/165),
-            child: Container(
-              width: data.size.width / 1.2,
-                  height: data.size.height / 17,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left:data.size.height/50,top:data.size.height/80,bottom:data.size.height/77 ),
-            child: FaIcon(icons,size: data.size.height/52,),
-          ),
-          Text(
-            text,
-            style: TextStyle(
-               
-                fontFamily: "Roboro",
-                fontSize: data.size.height / 59,letterSpacing:2,fontWeight: FontWeight.w400),
-                
-          ),
-          
-          Padding(
-            padding: EdgeInsets.only(right:data.size.height/77,top:data.size.height/70,bottom:data.size.height/77 ),
-            child: Icon(
-              Icons.arrow_forward_ios,
-              size: data.size.height/42,
-              color: kActiveIconColor,
-            ),
-          ),
-        ],
+      padding: EdgeInsets.all(
+        data.size.height / 455,
       ),
+      child: Card(
+        shape: kCardShape,
+        elevation: 1.0,
+        child: Padding(
+          padding: EdgeInsets.only(top: data.size.height / 165),
+          child: Container(
+            width: data.size.width / 1.2,
+            height: data.size.height / 17,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: data.size.height / 50,
+                      top: data.size.height / 80,
+                      bottom: data.size.height / 77),
+                  child: FaIcon(
+                    icons,
+                    size: data.size.height / 52,
+                  ),
+                ),
+                Text(
+                  text,
+                  style: TextStyle(
+                      fontFamily: "Roboro",
+                      fontSize: data.size.height / 59,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.w400),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: data.size.height / 77,
+                      top: data.size.height / 70,
+                      bottom: data.size.height / 77),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    size: data.size.height / 42,
+                    color: kActiveIconColor,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
+      ),
     );
   }
 }
